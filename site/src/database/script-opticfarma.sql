b7cea9d249b876eca0c36ec157abc8189ea83da6
HeloSalgado <heloisa.salgado@sptech.school>
2023-12-13 11:14:52 -0300

    Adicionando modelagem e script alterado

--- site/src/database/script-opticfarma.sql
+++ site/src/database/script-opticfarma.sql
@@ -6,57 +6,48 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE OpticFarma;
 
-USE aquatech;
+USE OpticFarma;
 
 CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_empresa INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
 	cnpj CHAR(14)
 );
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table sensor (
+	esteira INT AUTO_INCREMENT,
+	tipo char(6),
+	fk_empresa INT,    
+    PRIMARY KEY (esteira, fk_empresa),
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa)
 );
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
 create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+	id_leitura INT PRIMARY KEY AUTO_INCREMENT,
+	leitura int,
+    dt_atual  DATETIME DEFAULT CURRENT_TIMESTAMP,
+	fk_sensor INT,
+	FOREIGN KEY (fk_sensor) REFERENCES sensor(esteira)
 );
 
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+insert into empresa (razao_social, cnpj) values ('Ultrafarma', '42157895684125');
+insert into sensor (tipo, fk_empresa) values ('Óptico', 1);
+
+select * from empresa;
+select * from sensor;
 
 /*
 comando para sql server - banco remoto - ambiente de produção
